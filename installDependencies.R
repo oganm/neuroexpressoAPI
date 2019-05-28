3e4752e64ea34913abc3453f00285e144bbb4f94
OganM <ogan.mancarcii@gmail.com>
2019-05-27 17:42:22 -0700

    create a local dependency directory

--- installDependencies.R
+++ installDependencies.R
@@ -1,5 +1,10 @@
+dir.create('packages',showWarnings = FALSE)
+libPath = normalizePath('packages')
+
+.libPaths(libPath)
+
 if(!'devtools' %in% installed.packages()[,1]){
-    install.packages('devtools')
+    install.packages('devtools',lib = libPath)
 }
 
 dependencies = readLines('depends.txt')
@@ -9,8 +14,8 @@ remote = dependencies[grepl('/',dependencies)]
 
 installed = installed.packages()
 
-install.packages(dependencies[!dependencies %in% installed])
+install.packages(dependencies[!dependencies %in% installed],lib = libPath)
 
 for(package in remote){
-    devtools::install_github(package)
+    devtools::install_github(package,lib = libPath)
 }
